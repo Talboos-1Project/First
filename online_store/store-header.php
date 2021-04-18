33ab021ed9488fe51004c7d5686e2c941740637e
Abdulrazzak Shaker <abdulrazzak.shami@gmail.com>
2021-04-18 15:15:27 +0200

    Abdul

---
+++ online_store/store-header.php
@@ -0,0 +1,2 @@
+<!-- Latest compiled and minified CSS Bootstrap-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
---
+++ online_store/store.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php include("store-header.php"); ?>
+    <title>Online Store</title>
+  </head>
+  <body>
+    <section>
+      <?php include("section-1.php"); ?>
+    </section>
+
+  </body>
+</html>
