68442d50d7c52fde2151c054047eb531a5acbbcc
Abdulrazzak Shaker <abdulrazzak.shami@gmail.com>
2021-04-24 17:39:59 +0200

    side-Bar and fixed Nav-Bar

--- online_store/store-header.php
+++ online_store/store-header.php
@@ -4,9 +4,11 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css", integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l", crossorigin="anonymous")>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js%22%3E">
+    <link rel="stylesheet" href="../css/css_Store/store.css">
+
 
     <title>Online Store</title>
 
@@ -22,5 +24,4 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
 </head>
--- online_store/store.php
+++ online_store/store.php
@@ -2,16 +2,18 @@
 <?php include "navbar_header.php";?>
 
 <body>
-<div class="container">
-    <div class="container">
-        <?php include "section-1.php"?>
-        <?php include "section-2.php"?>
-        <?php include "section-3.php"?>
-    </div>
-    <div class="container">
+<div class="container-fluid" id= "main-container">
+    <div class="col-12 col-md-3 col-xl-2 bd-sidebar">
         <?php include "sidebar.php"?>
     </div>
+    <div class="d-none d-xl-block col-xl-2 bd-toc">
 
+    </div>
+    <main class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
+      <?php include "section-1.php"?>
+      <?php include "section-2.php"?>
+      <?php include "section-3.php"?>
+    </main>
 </div>
 
 
