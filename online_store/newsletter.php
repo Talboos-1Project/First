36238172d85a853742a60955671b61487941f39f
NourAldeenKassar <nn.aldeen@gmx.net>
2021-04-25 15:14:12 +0200

    Added Newsletter signUp

---
+++ online_store/newsletter.php
@@ -0,0 +1,53 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <img class="img-fluid" src="../images/bnw.jpg" alt="background pic" width="500" height="600">
+        </div>
+        <div class="col-sm-6">
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputFirstName">First Name</label>
+                        <input type="text" class="form-control" id="inputFirstName" placeholder="First Name">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputLastName">Last Name</label>
+                        <input type="text" class="form-control" id="inputLastName" placeholder="Last Name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail">Email Address</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword"  placeholder="your password">
+                </div>
+                <div class="form-group">
+                    <label for="">Enter password again</label>
+                    <input type="password" class="form-control" placeholder="your password">
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputCity">City</label>
+                        <input type="text" class="form-control" id="inputCity">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="inputState">State</label>
+                        <select id="inputState" class="form-control">
+                            <option selected>Choose...</option>
+                            <option>...</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="inputZip">Zip</label>
+                        <input type="text" class="form-control" id="inputZip">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-dark">Sign up</button>
+            </form>
+        </div>
+    </div>
+</div>
+
--- online_store/store.php
+++ online_store/store.php
@@ -12,7 +12,7 @@
     <main class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
       <?php include "section-1.php"?>
       <?php include "section-2.php"?>
-      <?php include "section-3.php"?>
+      <?php include "newsletter.php" ?>
     </main>
 </div>
 
