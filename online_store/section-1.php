481fba36e9ec7de3885d18070b59bf8cfbe887d6
Abdulrazzak Shaker <abdulrazzak.shami@gmail.com>
2021-04-25 16:00:46 +0200

    added 3 images in section-1

--- online_store/section-1.php
+++ online_store/section-1.php
@@ -1,4 +1,48 @@
 <div class="container p-3 my-3 bg-dark text-white">
-    <h1>My First Bootstrap Page</h1>
-    <p>This container has a dark background color and a white text, and some extra padding and margins.</p>
+  <h2>Carousel Example</h2>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol> -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <!-- <img src="../images/Khaled.jpg" class="img-fluid" alt="Abdul" style="height : 600px;"> -->
+        <img src="../images/6.jpg" alt="Los Angeles" class="carousel-image">
+      </div>
+
+      <div class="item">
+        <!-- <img src="../images/Captain.jpg" class="img-fluid" alt="Abdul" style="height : 600px;"> -->
+        <img src="../images/8.jpg" alt="Chicago" class="carousel-image">
+        <div class="carousel-caption">
+          <h3>Oline Shop</h3>
+          <p>find your own choice wherever you are</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <!-- <img src="../images/Captain.jpg" class="img-fluid" alt="Abdul" style="height : 600px;"> -->
+        <img src="../images/4.jpg" alt="San Fransisco" class="carousel-image">
+        <div class="carousel-caption" style="">
+          <h3>Membership</h3>
+          <p>Regester and take the advantages of beeing a member</p>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a> -->
+  </div>
 </div>
